e777471b029e117442be89ecd2fc55c0f3fca8cb
cbaetti <cbaetti@evoltis.com>
2021-10-13 10:05:21 -0300

    sprint 0 angular LS61

---
+++ src/app/configurations/containers/subsidiaries/subsidiaries.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subsidiaries',
+  templateUrl: './subsidiaries.component.html',
+  styleUrls: ['./subsidiaries.component.scss'],
+})
+export class SubsidiariesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
