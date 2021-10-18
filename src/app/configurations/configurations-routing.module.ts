eac115e7098f620ec0eebc890369ae23d57db8e5
Lucas Gordillo <lucasdgordillo@gmail.com>
2021-10-17 22:04:52 -0300

    Creacion de tabs + fix routing modules

--- src/app/configurations/configurations-routing.module.ts
+++ src/app/configurations/configurations-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigurationsPage } from './containers/configurations/configurations.page';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: ConfigurationsPage
+  },
+  // {
+  //   path: 'user-list',
+  //   component: UserListPage
+  // },
 ];
 @NgModule({
   imports: [
--- src/app/configurations/configurations.module.ts
+++ src/app/configurations/configurations.module.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
+import { SharedModule } from "../shared/shared.module";
+import { ConfigurationsRoutingModule } from "./configurations-routing.module";
+import { ConfigurationsPage } from "./containers/configurations/configurations.page";
 
 @NgModule({
-  declarations: [],
+  declarations: [ ConfigurationsPage ],
   imports: [
     CommonModule,
     IonicModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    ConfigurationsRoutingModule,
+    SharedModule
   ]
 })
 
