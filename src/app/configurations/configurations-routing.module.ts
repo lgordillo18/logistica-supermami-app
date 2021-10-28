a164c7942b2ef50ef51bc758c99ab3d7410f7092
Lucas Gordillo <lucasdgordillo@gmail.com>
2021-10-28 00:37:12 -0300

    .

--- src/app/configurations/configurations-routing.module.ts
+++ src/app/configurations/configurations-routing.module.ts
@@ -1,13 +1,9 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConfigurationsPage } from './containers/configurations/configurations.page';
 import { UsersListPage } from './containers/user-list/user-list.page';
-<<<<<<< HEAD
-=======
 import { VehicleListPage } from './containers/vehicle-list/vehicle-list.page';
-import { UsersPage } from './containers/user/user.page';
 import { VehiclePage } from './containers/vehicle/vehicle.page';
->>>>>>> f6c133da30c54547115415a2524ec9d490333362
 
 const routes: Routes = [
   {
@@ -17,21 +13,14 @@ const routes: Routes = [
   {
     path: 'user-list',
     component: UsersListPage
-<<<<<<< HEAD
-=======
   },
   {
     path: 'vehicle-list',
     component: VehicleListPage
   },
   {
-    path: 'create-user',
-    component: UsersPage
-  },
-  {
     path: 'create-vehicle',
     component: VehiclePage
->>>>>>> f6c133da30c54547115415a2524ec9d490333362
   }
 ];
 @NgModule({
