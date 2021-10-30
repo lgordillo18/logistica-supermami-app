259d17a7241e90deaeba6b822505e9442e333952
elponsis <111057@tecnicatura.frc.utn.edu.ar>
2021-10-30 00:13:18 -0300

    vehiclesEmployeeFixed

--- src/app/configurations/containers/vehicle-list/vehicle-list.page.ts
+++ src/app/configurations/containers/vehicle-list/vehicle-list.page.ts
@@ -1,51 +1,108 @@
 import { Component, OnInit } from '@angular/core';
-import { VehicleService } from '../../services/vehicles.service';
+import { AlertController, LoadingController, ModalController } from '@ionic/angular';
+import { NewVehicle } from 'src/app/models/vehicle';
+import { VehicleComponent } from '../../components/vehicle/vehicle.component';
+import { ListArray } from '../../models/list-array.interface';
+import { VehicleService } from '../../services/vehicle.service';
 
 @Component({
-  selector: 'app-vehicle-list',
+  selector: 'vehicle-list-page',
   templateUrl: './vehicle-list.page.html',
-  styleUrls: ['./vehicle-list.page.scss'],
+  styleUrls: ['./vehicle-list.page.scss']
 })
-export class VehicleListPage implements OnInit {
-  public vehicleList: any[];
-  public textConfig = { primaryText: 'full_name', secondaryText: 'username', tertiaryText: 'rol' };
-  constructor(private vehicleService: VehicleService) { }
+export class VehiclesListPage implements OnInit {
+  public arrayList: ListArray[] = [];
+  public loading: boolean;
+
+  constructor(
+    private vehiclesService: VehicleService,
+    private modalController: ModalController,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
-    this.vehicleList = [
-      {
-        id: 1,
-        patente: 'awd 123',
-        id_marca: 3,
-        id_modelo: 3,
-        anio: '1950',
-        id_estado: 2
-      },
-      {
-        id: 4,
-        patente: 'ggg 567',
-        id_marca: 2,
-        id_modelo: 1,
-        anio: '2001',
-        id_estado: 2
+    this.loading = true;
+    this.getVehicles();
+  }
+
+  editVehicle(vehicleId) {
+    this.vehiclesService.getVehicle(vehicleId).subscribe(async (vehicle) => {
+      this.openVehicleFormModal(vehicle);
+    });
+  }
+
+  async removeVehicle(vehicleId) {
+    this.vehiclesService.deleteVehicle(vehicleId).subscribe(async (response) => {
+      if (response) {
+        this.arrayList = this.arrayList.filter(item => item.id == vehicleId);  
       }
-    ];
+    });
   }
 
-  editVehicle(userId) {
-    // Aca iria el endpoint para editar
+  async showDeleteAlert(vehicleId) {
+    const alert = await this.alertController.create({
+      header: 'Eliminar vehiculo',
+      message: '¿Esta seguro de eliminar este vehiculo?',
+      buttons: [
+        { text: 'Cancelar', role: 'cancel' },
+        { text: 'OK', cssClass: 'danger', handler: () => { this.removeVehicle(vehicleId) }}
+      ],
+      backdropDismiss: false
+    });
+    await alert.present();
   }
 
-  removeVehicle(userId) {
-    // Aca iria el endpoint para borrar
+  async openVehicleFormModal(vehicleData = null) {
+    const modal = await this.modalController.create({
+      component: VehicleComponent,
+      componentProps: {
+        vehicleData: vehicleData
+      }
+    });
+
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+
+    if (data) {
+      if (data.action === 'new' && data.response) {
+        this.createVehicle(data.response);
+      }
+    }
   }
 
-  // getVehiculos() {
-  //   this.usersService.getUsers().subscribe(users => {
-  //     this.userList = users;
-  //     console.log(this.users);
-  //   })
-  // }
+  private async createVehicle(newVehicle) {
+    this.vehiclesService.newVehicle(new NewVehicle(newVehicle)).subscribe(async (response) => {
+      if (response) {
+        this.addVehicle(response);
+      }
+    });
+  }
+
+  setVehicleArrayList(vehicles) {
+    vehicles.forEach(vehicle => {
+      if (!vehicle.deleted) {
+        this.arrayList.push({ id: vehicle.id, primaryText: vehicle.vehicleBrand, secondaryText: vehicle.vehicleModel, tertiaryText: vehicle.patent });  
+      }
+    });
+  }
+
+  private async getVehicles() {
+    this.vehiclesService.getVehicles().subscribe(async (vehicles) => {
+      if (vehicles) {
+        this.setVehicleArrayList(vehicles);
+        this.loading = false;
+      }
+    });
+  }
+
+  // Agrego el vehicle al array para poder verlo sin necesidad de recargar toda la pagina
+  addVehicle(newVehicle) {
+    this.arrayList.push({ id: newVehicle.id, primaryText: newVehicle.vehicleBrand, secondaryText: newVehicle.vehicleModel, tertiaryText: newVehicle.patent });
+  }
 }
+
+
 
 
