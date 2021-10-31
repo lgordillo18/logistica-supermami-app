6bc59dcc3e1043d7dda0e49f35ab67fc86df6872
Your Name <you@example.com>
2021-10-31 17:45:33 -0300

    UI Detalles ticket

--- src/app/tickets/tickets-routing.module.ts
+++ src/app/tickets/tickets-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TicketDetailsPage } from './containers/ticket-details/ticket-details.page';
 import { TicketsPage } from './containers/tickets/tickets.page';
 const routes: Routes = [
     {
       path: '',
       component: TicketsPage
+    },
+    {
+      path: 'ticket-details',
+      component: TicketDetailsPage
     }
 ];
 @NgModule({
--- src/app/tickets/tickets.module.ts
+++ src/app/tickets/tickets.module.ts
@@ -7,12 +7,16 @@ import { TicketsPage } from "./containers/tickets/tickets.page";
 import { TicketListComponent } from "./components/ticket-list/ticket-list.component";
 import { TicketsRoutingModule } from "./tickets-routing.module";
 import { TicketComponent } from "./components/ticket/ticket.component";
+import { TicketDetailsComponent } from "./components/ticket-details/ticket-details.component";
+import { TicketDetailsPage } from "./containers/ticket-details/ticket-details.page";
 
 @NgModule({
   declarations: [
     TicketListComponent,
     TicketsPage,
-    TicketComponent
+    TicketComponent,
+    TicketDetailsPage,
+    TicketDetailsComponent
   ],
   imports: [
     CommonModule,
